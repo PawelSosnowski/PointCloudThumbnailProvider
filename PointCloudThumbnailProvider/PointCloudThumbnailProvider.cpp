868d6f5e721d451acdbecfd60f175b737293a083
Karolina Serek <karolinamchtr@gmail.com>
2020-05-31 14:00:20 +0200

    Dodanie biblioteki, wczytanie streamu i pobranie wektora punktów z chmury

--- PointCloudThumbnailProvider/PointCloudThumbnailProvider.cpp
+++ PointCloudThumbnailProvider/PointCloudThumbnailProvider.cpp
@@ -1,7 +1,8 @@
+#include "happly.h"
 #include <Shlwapi.h>
 #include <thumbcache.h> // For IThumbnailProvider.
 #include <gdiplus.h>
-#include <new>
+#include <iostream>
 
 #pragma comment(lib, "shlwapi.lib")
 #pragma comment(lib, "gdiplus.lib")
@@ -49,11 +50,6 @@ public:
     IFACEMETHODIMP GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha) override;
 
 private:
-    HRESULT _FindThumbnailDataFromStream(ULONG* actualResourceDataSize);
-
-    HRESULT _GetThumbnailFromPointCloudThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha,
-        ULONG actualResourceDataSize);
-
     long _cRef;
     IStream* _pStream;     // provided during initialization.
 };
@@ -80,110 +76,60 @@ IFACEMETHODIMP CPointCloudThumbProvider::Initialize(IStream* pStream, DWORD) {
 
 // IThumbnailProvider
 IFACEMETHODIMP CPointCloudThumbProvider::GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha) {
-    ULONG actualResourceDataSize;
-    HRESULT hr = _FindThumbnailDataFromStream(&actualResourceDataSize);
+    *pdwAlpha = WTSAT_RGB;
+    STATSTG statstg;
+    HRESULT hr = _pStream->Stat(&statstg, STATFLAG_NONAME);
     if (SUCCEEDED(hr)) {
-        Gdiplus::GdiplusStartupInput startupInput;
-        ULONG_PTR gdiplusToken;
-        Gdiplus::GdiplusStartup(&gdiplusToken, &startupInput, nullptr);
+        auto fileSize = statstg.cbSize.QuadPart;
+        std::vector<char> buffer(fileSize + 1);
+        hr = _pStream->Read(buffer.data(), fileSize, nullptr);
+        if (SUCCEEDED(hr)) {
+            std::string ret(buffer.data());
+            std::stringstream ss;
+            ss << ret;
 
-        HRESULT hr_thumb = _GetThumbnailFromPointCloudThumbnail(cx, phbmp, pdwAlpha, actualResourceDataSize);
+            happly::PLYData plyIn(ss);
+            std::vector<std::array<double, 3>> vPos = plyIn.getVertexPositions();
 
-        Gdiplus::GdiplusShutdown(gdiplusToken); // shut down GDIPlus
+            // tu trzeba stworzyc te bitmape z tych punktow w vPos
 
-        return hr_thumb;
-    }
-    return E_FAIL;
-}
+            Gdiplus::GdiplusStartupInput startupInput;
+            ULONG_PTR gdiplusToken;
+            Gdiplus::GdiplusStartup(&gdiplusToken, &startupInput, nullptr);
+            Gdiplus::Bitmap* gdiPlusBitmap = new Gdiplus::Bitmap(256, 256, PixelFormat24bppRGB);
 
-ULONG ReadUInt32BE(IStream* pStream) {
-    unsigned char buf[4];
-    pStream->Read(buf, 4, nullptr);
+            gdiPlusBitmap->SetPixel(10, 10, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(11, 11, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(12, 12, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(13, 13, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(14, 14, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(15, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(16, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(17, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(18, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(19, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(20, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(21, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(22, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(23, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(24, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(25, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(26, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(27, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(28, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(29, 15, Gdiplus::Color::Red);
+            gdiPlusBitmap->SetPixel(30, 15, Gdiplus::Color::Red);
 
-    ULONG ret = ((buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | (buf[3] << 0));
-    return ret;
-}
+            Gdiplus::Status status = gdiPlusBitmap->GetHBITMAP(Gdiplus::Color::Black, phbmp);
+            delete gdiPlusBitmap;
+            if (status == Gdiplus::Status::Ok)
+                hr = S_OK;
+            else
+                hr = E_FAIL;
 
-ULONGLONG GetStreamPosition(IStream* pStream) {
-    LARGE_INTEGER seek_offset_zero = { 0 };
-    ULARGE_INTEGER currentPosition = { 0 };
-    pStream->Seek(seek_offset_zero, STREAM_SEEK_CUR, &currentPosition);
-    return currentPosition.QuadPart;
-}
-
-HRESULT CPointCloudThumbProvider::_FindThumbnailDataFromStream(ULONG* pActualResourceDataSize) {
-    LARGE_INTEGER seek_offset_zero = { 0 };
-    _pStream->Seek(seek_offset_zero, STREAM_SEEK_SET, nullptr);
-    {
-        // TODO: load .ply file
-        // https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/
-
-        // file header
-        LARGE_INTEGER seek_offset_file_header = { 4 + 2 + 6 + 2 + 4 + 4 + 2 + 2 };
-        _pStream->Seek(seek_offset_file_header, STREAM_SEEK_CUR, nullptr);
-
-        ULONG colorModeDataSectionLength = ReadUInt32BE(_pStream);
-        LARGE_INTEGER seek_offset_colorModeDataSectionLength = { colorModeDataSectionLength };
-        _pStream->Seek(seek_offset_colorModeDataSectionLength, STREAM_SEEK_CUR, nullptr);
-
-        ULONG imageResourcesSectionLength = ReadUInt32BE(_pStream);
-        ULONGLONG imageResourceFinishPos = GetStreamPosition(_pStream) + imageResourcesSectionLength;
-
-        while (GetStreamPosition(_pStream) != imageResourceFinishPos) {
-            if (GetStreamPosition(_pStream) > imageResourceFinishPos)
-                return E_FAIL;
-
-            // Signature: '8BIM'
-            unsigned char buf_signature[4];
-            _pStream->Read(buf_signature, 4, nullptr);
-
-            // Unique identifier for the resource.
-            unsigned char buf_imageResourceId[2];
-            _pStream->Read(buf_imageResourceId, 2, nullptr);
-
-            // 0x0409 (Photoshop 4.0) Thumbnail resource for Photoshop 4.0 only.See See Thumbnail resource format.
-            // 0x040C (Photoshop 5.0) Thumbnail resource (supersedes resource 1033). See See Thumbnail resource format.
-            bool isThumbnailResource = false;
-            if ((buf_imageResourceId[0] == 0x04 && buf_imageResourceId[1] == 0x09) ||
-                (buf_imageResourceId[0] == 0x04 && buf_imageResourceId[1] == 0x0C))
-                isThumbnailResource = true;
-
-            // Name: Pascal string, padded to make the size even (a null name consists of two bytes of 0)
-            unsigned char pascalStringLength;
-            _pStream->Read(&pascalStringLength, 1, nullptr);
-            LARGE_INTEGER seek_offset_pascalString = {
-                    static_cast<DWORD>(pascalStringLength + (pascalStringLength % 2 == 0 ? 1 : 0)) };
-            _pStream->Seek(seek_offset_pascalString, STREAM_SEEK_CUR, nullptr);
-
-            ULONG actualResourceDataSize = ReadUInt32BE(_pStream);  // Actual size of resource data that follows
-            if (!isThumbnailResource) {
-                // The resource data, described in the sections on the individual resource types.
-                // ** It is padded to make the size even. **
-                LARGE_INTEGER seek_offset_actualResource = { actualResourceDataSize + (actualResourceDataSize % 2) };
-                _pStream->Seek(seek_offset_actualResource, STREAM_SEEK_CUR, nullptr);
-            }
-            else {
-                *pActualResourceDataSize = actualResourceDataSize;
-                return S_OK;
-            }
+            Gdiplus::GdiplusShutdown(gdiplusToken); // shut down GDIPlus
+            return hr;
         }
-    }
-    return E_FAIL;
-}
-
-HRESULT CPointCloudThumbProvider::_GetThumbnailFromPointCloudThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha,
-    ULONG actualResourceDataSize) {
-    auto* buf_actualResourceData = new unsigned char[actualResourceDataSize];
-    _pStream->Read(buf_actualResourceData, actualResourceDataSize, nullptr);
-    IStream* memstream = SHCreateMemStream(buf_actualResourceData + 28, actualResourceDataSize - 28);
-    Gdiplus::Bitmap* gdiPlusBitmap = Gdiplus::Bitmap::FromStream(memstream);
-    memstream->Release();
-    delete[] buf_actualResourceData;
-
-    Gdiplus::Status status = gdiPlusBitmap->GetHBITMAP(Gdiplus::Color::Black, phbmp);
-    delete gdiPlusBitmap;
-    if (status == Gdiplus::Status::Ok) {
-        return S_OK;
     }
     return E_FAIL;
 }
